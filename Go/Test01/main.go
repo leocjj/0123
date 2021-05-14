512d30242b1d6d7800f2b5842c9418782822a0a5
leocjj <leocj@hotmail.com>
2021-05-13 21:26:49 -0500

    Update main.go

--- Go/Test01/main.go
+++ Go/Test01/main.go
@@ -8,9 +8,9 @@ import (
 )
 
 func power(base float64, expo float64) (float64, float64, float64) {
-	var power_of float64
-	power_of = math.Pow(base, expo)
-	return base, expo, power_of
+	var powerOf float64
+	powerOf = math.Pow(base, expo)
+	return base, expo, powerOf
 }
 
 func main() {
@@ -19,7 +19,7 @@ func main() {
 	n := 2548
 	fmt.Printf("%X\n", n)
 
-	//rune   Ex: var aRune rune = 'Z'. type rune = int32. Arune represent a Unicode code point. "U+X"
+	//rune   Ex: var aRune rune = 'Z'. type rune = int32. A rune represent a Unicode code point. "U+X"
 	//string   Ex : “management office”, “room 265”, `raw text`... implicitly encoded using UTF-8.
 	//uint, uint8, uint16, uint32, uint64   Ex : 2445, 676, 0, 1,...
 	//int, int8, int16, int32, int64   Ex : -1245, 65, 78,...
@@ -39,7 +39,10 @@ func main() {
 	// Constants
 	const version string = "1.3.1" // Typed constant
 	const version2 = "1.3.2"       // Untyped constant
-	fmt.Print(version + "\n")
+	fmt.Print(version, "\n")
+
+	abc := int32(15)
+	fmt.Println("This is abc: ", abc)
 
 	hotelName := "The Gopher Hotel"
 	fmt.Println("Hotel " + hotelName)
@@ -56,5 +59,5 @@ func main() {
 
 	var f, g, h = power(12, 2)
 	fmt.Printf("%0.2f %.0f %.2f", f, g, h)
-
+	fmt.Printf("%T %v %.2f", f, g, h)	// Type and value
 }
